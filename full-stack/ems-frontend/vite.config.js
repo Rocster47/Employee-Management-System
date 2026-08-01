bb69fb3c2024fa7d4721ae2d6287a50c1685feb7
Rocster47 <rocster2130@gmail.com>
2026-08-01 14:40:12 +0100

    Changed env variables to only be used in local environments.

--- full-stack/ems-frontend/vite.config.js
+++ full-stack/ems-frontend/vite.config.js
@@ -1,17 +1,30 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import fs from 'fs';
-import dotenv from 'dotenv';
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import fs from "fs";
 
-dotenv.config();
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    https: {
-      cert: fs.readFileSync(process.env.CERTIFICATE),
-      key: fs.readFileSync(process.env.CERT_KEY)
+  const useLocalHttps =
+    command === "serve" &&
+    env.CERTIFICATE &&
+    env.CERT_KEY &&
+    fs.existsSync(env.CERTIFICATE) &&
+    fs.existsSync(env.CERT_KEY);
+
+  return {
+    plugins: [react()],
+
+    server: {
+      port: 3000,
+      ...(useLocalHttps
+        ? {
+            https: {
+              cert: fs.readFileSync(env.CERTIFICATE),
+              key: fs.readFileSync(env.CERT_KEY),
+            },
+          }
+        : {}),
     },
-    port: 3000
-  }
+  };
 });
